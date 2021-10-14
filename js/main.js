1f17c64474830e66e8fe35201b41f448029c0879
bitreit71 <45029800+bitreit71@users.noreply.github.com>
2021-10-14 11:50:16 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,57 @@
+// Убавляем кол-во по клику
+    $('.quantity_inner .bt_minus').click(function() {
+    let $input = $(this).parent().find('.quantity');
+    let count = parseInt($input.val()) - 1;
+    count = count < 1 ? 1 : count;
+    $input.val(count);
+});
+// Прибавляем кол-во по клику
+$('.quantity_inner .bt_plus').click(function() {
+    let $input = $(this).parent().find('.quantity');
+    let count = parseInt($input.val()) + 1;
+    count = count > parseInt($input.data('max-count')) ? parseInt($input.data('max-count')) : count;
+    $input.val(parseInt(count));
+});
+// Убираем все лишнее и невозможное при изменении поля
+$('.quantity_inner .quantity').bind("change keyup input click", function() {
+    if (this.value.match(/[^0-9]/g)) {
+        this.value = this.value.replace(/[^0-9]/g, '');
+    }
+    if (this.value == "") {
+        this.value = 1;
+    }
+    if (this.value > parseInt($(this).data('max-count'))) {
+        this.value = parseInt($(this).data('max-count'));
+    }
+});
+
+
+
+  $(document).ready(function(){
+  $("a[href*=#]").on("click", function(e){
+    var anchor = $(this);
+    $('html, body').stop().animate({
+      scrollTop: $(anchor.attr('href')).offset().top
+    }, 777);
+    e.preventDefault();
+    return false;
+  });
+});
+
+
+
+ // scrollAnc
+ const anchors = document.querySelectorAll('a[href*="#"]')
+
+ for (let anchor of anchors) {
+   anchor.addEventListener('click', function (e) {
+     e.preventDefault()
+
+     const blockID = anchor.getAttribute('href').substr(1)
+
+     document.getElementById(blockID).scrollIntoView({
+       behavior: 'smooth',
+       block: 'start'
+     })
+   })
+ }
